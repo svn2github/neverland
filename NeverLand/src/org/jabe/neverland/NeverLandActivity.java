ce5b8eb3c2f7eca00d2202c2326be6e6f6f61070
jabelai1523@gmail.com <jabelai1523@gmail.com@e8d482b9-9a47-ca14-e2bd-9143b6004eba>
2012-05-04 03:24:47 +0000

    For Test
    git-svn-id: http://neverland.googlecode.com/svn/trunk@12 e8d482b9-9a47-ca14-e2bd-9143b6004eba

--- NeverLand/src/org/jabe/neverland/NeverLandActivity.java
+++ NeverLand/src/org/jabe/neverland/NeverLandActivity.java
@@ -9,6 +9,5 @@ public class NeverLandActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
     }
 }
