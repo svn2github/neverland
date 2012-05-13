9437d69d6c6d88ccc32829728710ae2876f8fa88
jabelai1523@gmail.com <jabelai1523@gmail.com@e8d482b9-9a47-ca14-e2bd-9143b6004eba>
2012-05-13 11:45:47 +0000

    git-svn-id: http://neverland.googlecode.com/svn/trunk@47 e8d482b9-9a47-ca14-e2bd-9143b6004eba

--- NeverLand/src/org/jabe/neverland/model/ListItem.java
+++ NeverLand/src/org/jabe/neverland/model/ListItem.java
@@ -7,8 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public abstract class ListItem<T> implements ListElement<T>, View.OnClickListener {
-	
+public abstract class ListItem<T> implements ListElement<T>,
+		View.OnClickListener {
+
 	protected List<T> mLists;
 	protected Context mContext;
 
@@ -16,27 +17,32 @@ public abstract class ListItem<T> implements ListElement<T>, View.OnClickListene
 		this.mContext = context;
 		this.mLists = list;
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.jabe.neverland.model.ListElement#getView(int, android.view.View, android.view.ViewGroup)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jabe.neverland.model.ListElement#getView(int, android.view.View,
+	 * android.view.ViewGroup)
 	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = convertView;
 		IViewHolder holder = null;
 		if (useConvertView()) {
-			//缓存ViewHolder
+			// 缓存ViewHolder
 			if (view == null) {
-				//耗时操作inflate
-				view = LayoutInflater.from(mContext).inflate(getLayoutId(), null);
+				// 耗时操作inflate
+				view = LayoutInflater.from(mContext).inflate(getLayoutId(),
+						null);
 				holder = getViewHolder();
 				holder.saveView(view, position, true);
 			} else {
-				//获取缓存holder
-				holder =  (IViewHolder) view.getTag();
+				// 获取缓存holder
+				holder = (IViewHolder) view.getTag();
 				if (holder == null) {
-					//耗时操作inflate
-					view = LayoutInflater.from(mContext).inflate(getLayoutId(), null);
+					// 耗时操作inflate
+					view = LayoutInflater.from(mContext).inflate(getLayoutId(),
+							null);
 					holder = getViewHolder();
 					holder.saveView(view, position, true);
 				} else {
@@ -44,21 +50,23 @@ public abstract class ListItem<T> implements ListElement<T>, View.OnClickListene
 				}
 			}
 		} else {
-			//inflate,但是不缓存
+			// inflate,但是不缓存
 			view = LayoutInflater.from(mContext).inflate(getLayoutId(), null);
 			holder = getViewHolder();
 			holder.saveView(view, position, false);
 		}
-		//do something for holder~~~
+		// do something for holder~~~
 		refreshHolder(holder, position);
 		return view;
 	}
+
 	public abstract ListElement<T> clone();
+
 	public abstract void click(View v);
 
 	@Override
 	public List<T> getDataList() {
-		
+
 		return mLists;
 	}
 
@@ -76,7 +84,7 @@ public abstract class ListItem<T> implements ListElement<T>, View.OnClickListene
 	public void insert(T data, int position) {
 		mLists.add(position, data);
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		this.click(v);
