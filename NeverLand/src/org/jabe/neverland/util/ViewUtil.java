26f96ce9038adb0716cc37bec260eb4b3d171d7c
jabelai1523@gmail.com <jabelai1523@gmail.com@e8d482b9-9a47-ca14-e2bd-9143b6004eba>
2012-11-14 08:44:11 +0000

    git-svn-id: http://neverland.googlecode.com/svn/trunk@82 e8d482b9-9a47-ca14-e2bd-9143b6004eba

--- NeverLand/src/org/jabe/neverland/util/ViewUtil.java
+++ NeverLand/src/org/jabe/neverland/util/ViewUtil.java
@@ -56,4 +56,63 @@ public class ViewUtil {
 		activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
 		return frame;
 	}
+	
+
+	/**
+	 * FLAG_NOT_TOUCH_MODAL : view本身可以响应touch事件,不在view范围的内的touch事件也可以被传递.
+	 * FLAG_NOT_FOCUSABLE   : view将不获取焦点,点击事件将不被响应.
+	 * 
+	 * 组合之后: 可以响应touch事件,不能响应key事件.
+	 * 
+	 * @return
+	 */
+	public static WindowManager.LayoutParams getLeftTopWindowLayout() {
+		WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+		params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+				| WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;
+		params.flags = LayoutParams.FLAG_NOT_TOUCH_MODAL
+				| LayoutParams.FLAG_NOT_FOCUSABLE;
+		params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+		params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+		params.alpha = 100;
+		params.gravity = Gravity.LEFT | Gravity.TOP;
+		params.format = PixelFormat.RGBA_8888;
+		// 以屏幕左上角为原点，设置x、y初始值
+		params.x = 0;
+		params.y = 0;
+		return params;
+	}
+
+	public static WindowManager.LayoutParams getCenterWindowParams() {
+		WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+		params.type = WindowManager.LayoutParams.TYPE_PHONE;
+		params.flags = LayoutParams.FLAG_BLUR_BEHIND|0x40000000;
+		params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+		params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+		params.alpha = 100;
+		params.gravity = Gravity.CENTER;
+		params.format = PixelFormat.RGBA_8888;
+		return params;
+	}
+
+	public static WindowManager.LayoutParams addViewToTop(Context context,
+			View view, WindowManager.LayoutParams params) {
+		WindowManager wm = (WindowManager) context.getApplicationContext()
+				.getSystemService(WINDOW_SERVICE);
+		wm.addView(view, params);
+		return params;
+	}
+
+	public static void updateTopView(Context context, View view,
+			WindowManager.LayoutParams params) {
+		WindowManager wm = (WindowManager) context.getApplicationContext()
+				.getSystemService(WINDOW_SERVICE);
+		wm.updateViewLayout(view, params);
+	}
+
+	public static void removeViewInTop(Context context, View view) {
+		WindowManager wm = (WindowManager) context.getApplicationContext()
+				.getSystemService(WINDOW_SERVICE);
+		wm.removeView(view);
+	}
 }
