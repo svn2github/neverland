45fabc7d31b964f0943010ce33cb60432dbc8c8d
jabelai1523@gmail.com <jabelai1523@gmail.com@e8d482b9-9a47-ca14-e2bd-9143b6004eba>
2012-12-12 06:07:03 +0000

    git-svn-id: http://neverland.googlecode.com/svn/trunk@94 e8d482b9-9a47-ca14-e2bd-9143b6004eba

---
+++ NeverLand/src/org/jabe/neverland/energycurve/MainActivity.java
@@ -0,0 +1,39 @@
+﻿package org.jabe.neverland.energycurve;
+
+import java.util.ArrayList;
+
+import org.jabe.neverland.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    private EnergyCurveView erenergyCurve;
+    private DisplayMetrics dm = new DisplayMetrics();
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        erenergyCurve = (EnergyCurveView) findViewById(R.id.erenergycurve);
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        erenergyCurve.setWindowsWH(dm);
+        ArrayList<EnergyItem> energys = new ArrayList<EnergyItem>();
+        energys.add(new EnergyItem("1", 0.0f, "无"));
+        energys.add(new EnergyItem("2", 0.0f, "无"));
+        energys.add(new EnergyItem("3", 8.0f, "无"));
+        energys.add(new EnergyItem("4", 6.0f, "无"));
+        energys.add(new EnergyItem("5", 2.8f, "无"));
+        energys.add(new EnergyItem("6", 0.0f, "无"));
+        energys.add(new EnergyItem("7", 10.0f, "无"));
+        erenergyCurve.setData(energys, EnergyType.DAY);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
