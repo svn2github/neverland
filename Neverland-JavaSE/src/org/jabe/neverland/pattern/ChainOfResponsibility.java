b50867de36c8e08bb641730679a8183061db8eb4
jabelai1523@gmail.com <jabelai1523@gmail.com@e8d482b9-9a47-ca14-e2bd-9143b6004eba>
2013-01-05 10:29:56 +0000

    git-svn-id: http://neverland.googlecode.com/svn/trunk@113 e8d482b9-9a47-ca14-e2bd-9143b6004eba

--- Neverland-JavaSE/src/org/jabe/neverland/pattern/ChainOfResponsibility.java
+++ Neverland-JavaSE/src/org/jabe/neverland/pattern/ChainOfResponsibility.java
@@ -2,5 +2,76 @@ package org.jabe.neverland.pattern;
 
 public class ChainOfResponsibility {
 
-	
+	public interface RequestHandle {
+
+		public void handleRequest(Request request);
+
+	}
+
+	public interface Request {
+		public String getRequestContent();
+	}
+
+	public static class HttpHandle implements RequestHandle {
+
+		@Override
+		public void handleRequest(Request request) {
+			System.out.println("HttpHandle handle the message : "
+					+ request.getRequestContent());
+		}
+
+	}
+
+	public static class FTPHandle implements RequestHandle {
+
+		@Override
+		public void handleRequest(Request request) {
+			System.out.println("FTPHandle handle the message : "
+					+ request.getRequestContent());
+		}
+
+	}
+
+	public static class HttpRequest implements Request {
+
+		@Override
+		public String getRequestContent() {
+			// TODO Auto-generated method stub
+			return "Http: message";
+		}
+
+	}
+
+	public static class FTPRequeset implements Request {
+
+		@Override
+		public String getRequestContent() {
+			return "FTP: message";
+		}
+
+	}
+
+	public static void main(String[] args) {
+		
+		final Request request1 = new FTPRequeset();
+		final Request request2 = new HttpRequest();
+		final ChainOfResponsibility app = new ChainOfResponsibility();
+		app.handleRequest(request1);
+		app.handleRequest(request2);
+		
+	}
+
+	public void handleRequest(Request request) {
+		
+		if (request.getRequestContent().startsWith("Http")
+				|| request instanceof HttpRequest) {
+			new HttpHandle().handleRequest(request);
+		}
+
+		if (request.getRequestContent().startsWith("FTP")
+				|| request instanceof FTPRequeset) {
+			new FTPHandle().handleRequest(request);
+		}
+		
+	}
 }
