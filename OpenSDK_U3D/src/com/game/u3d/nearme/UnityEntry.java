296953de426a5b2bf1612b8e617adc6c641e2e7f
jabelai1523@gmail.com <jabelai1523@gmail.com@e8d482b9-9a47-ca14-e2bd-9143b6004eba>
2013-03-28 17:58:08 +0000

    git-svn-id: http://neverland.googlecode.com/svn/trunk@132 e8d482b9-9a47-ca14-e2bd-9143b6004eba

--- OpenSDK_U3D/src/com/game/u3d/nearme/UnityEntry.java
+++ OpenSDK_U3D/src/com/game/u3d/nearme/UnityEntry.java
@@ -20,36 +20,50 @@ public class UnityEntry extends UnityPlayerActivity {
 	}
 
 	public void SDK_init() {
-		// 测试用的appkey和secret
-		GameCenterSettings gameCenterSettings = new GameCenterSettings(
-				"c5217trjnrmU6gO5jG8VvUFU0", "e2eCa732422245E8891F6555e999878B") {
-
+		this.runOnUiThread(new Runnable() {
+			
 			@Override
-			public void onForceReLogin() {
-				// SDK由于某些原因登出,此方法通知cp,cp需要在此处清理当前的登录状态并重新请求登录.
+			public void run() {
+				// TODO Auto-generated method stub
+				// 测试用的appkey和secret
+				GameCenterSettings gameCenterSettings = new GameCenterSettings(
+						"c5217trjnrmU6gO5jG8VvUFU0", "e2eCa732422245E8891F6555e999878B") {
+
+					@Override
+					public void onForceReLogin() {
+						// SDK由于某些原因登出,此方法通知cp,cp需要在此处清理当前的登录状态并重新请求登录.
+					}
+				};
+				GameCenterSettings.isDebugModel = true;// 测试log开关
+				GameCenterSettings.isOritationPort = true;// 竖屏
+				// GameCenterSettings.isNeedShowLoading = false;
+				// GameCenterSettings.request_time_out = 10000;//设置SDK网络
+				GameCenterSDK.init(gameCenterSettings, UnityEntry.this);
 			}
-		};
-		GameCenterSettings.isDebugModel = true;// 测试log开关
-		GameCenterSettings.isOritationPort = true;// 竖屏
-		// GameCenterSettings.isNeedShowLoading = false;
-		// GameCenterSettings.request_time_out = 10000;//设置SDK网络
-		GameCenterSDK.init(gameCenterSettings, this);
+		});
 	}
 
 	public void SDK_doLogin() {
 		GameCenterSDK.setmCurrentContext(this);
-		GameCenterSDK.getInstance().doLogin(new ApiCallback() {
-
+		runOnUiThread(new Runnable() {
+			
 			@Override
-			public void onSuccess(String content, int code) {
-				UnityPlayer.UnitySendMessage("Main", "onNearMeLoginCallback",
-						code + "#" + content);
-			}
+			public void run() {
+				// TODO Auto-generated method stub
+				GameCenterSDK.getInstance().doLogin(new ApiCallback() {
 
-			@Override
-			public void onFailure(String content, int code) {
-				UnityPlayer.UnitySendMessage("Main", "onNearMeLoginCallback",
-						code + "#" + content);
+					@Override
+					public void onSuccess(String content, int code) {
+						UnityPlayer.UnitySendMessage("Main", "onNearMeLoginCallback",
+								code + "#" + content);
+					}
+
+					@Override
+					public void onFailure(String content, int code) {
+						UnityPlayer.UnitySendMessage("Main", "onNearMeLoginCallback",
+								code + "#" + content);
+					}
+				});
 			}
 		});
 	}
