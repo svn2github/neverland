68ac4447024fa5d17332e80d54cc2b2ae026e3ec
jabelai1523@gmail.com <jabelai1523@gmail.com@e8d482b9-9a47-ca14-e2bd-9143b6004eba>
2013-08-09 12:21:23 +0000

    git-svn-id: http://neverland.googlecode.com/svn/trunk@159 e8d482b9-9a47-ca14-e2bd-9143b6004eba

---
+++ NeverLand/src/com/mobeta/android/demodslv/BGHandle.java
@@ -0,0 +1,16 @@
+package com.mobeta.android.demodslv;
+
+import org.jabe.neverland.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class BGHandle extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bg_handle_main);
+    }
+
+}
---
+++ NeverLand/src/com/mobeta/android/demodslv/CursorDSLV.java
@@ -0,0 +1,41 @@
+package com.mobeta.android.demodslv;
+
+import org.jabe.neverland.R;
+
+import android.database.MatrixCursor;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.mobeta.android.dslv.DragSortListView;
+import com.mobeta.android.dslv.SimpleDragSortCursorAdapter;
+
+
+public class CursorDSLV extends FragmentActivity {
+
+    private SimpleDragSortCursorAdapter adapter;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cursor_main);
+
+        String[] cols = {"name"};
+        int[] ids = {R.id.text};
+        adapter = new SimpleDragSortCursorAdapter(this,
+                R.layout.list_item_click_remove, null, cols, ids, 0);
+
+        DragSortListView dslv = (DragSortListView) findViewById(android.R.id.list);
+        dslv.setAdapter(adapter);
+
+        // build a cursor from the String array
+        MatrixCursor cursor = new MatrixCursor(new String[] {"_id", "name"});
+        String[] artistNames = getResources().getStringArray(R.array.jazz_artist_names);
+        for (int i = 0; i < artistNames.length; i++) {
+            cursor.newRow()
+                    .add(i)
+                    .add(artistNames[i]);
+        }
+        adapter.changeCursor(cursor);
+    }
+}
