5f25112d853c2d31b93e792c4ae17417a4742cc6
jabelai1523@gmail.com <jabelai1523@gmail.com@e8d482b9-9a47-ca14-e2bd-9143b6004eba>
2014-01-09 03:08:45 +0000

    git-svn-id: http://neverland.googlecode.com/svn/trunk@217 e8d482b9-9a47-ca14-e2bd-9143b6004eba

--- NeverLandJni/obj/local/armeabi/objs-debug/GCJniTool/com_example_hellojni_HelloJni.o.d
+++
@@ -1,7 +0,0 @@
-obj/local/armeabi/objs-debug/GCJniTool/com_example_hellojni_HelloJni.o: \
- jni/com_example_hellojni_HelloJni.c jni/com_example_hellojni_HelloJni.h \
- E:/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/jni.h
-
-jni/com_example_hellojni_HelloJni.h:
-
-E:/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/jni.h:
